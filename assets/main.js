1907bec5abc56c3ed989d315fcce7c29f532af4d
alexellevu <newnapster83@gmail.com>
2021-05-18 17:23:31 +0200

    creato slider con immagini

---
+++ assets/main.js
@@ -0,0 +1,32 @@
+const root = new Vue({
+    el:'#root',
+    data:{
+        counter:0,
+        images:['https://euro.venum.com//media/wysiwyg/loma/desk/VISUEL_SLIDER_03_02.jpg',
+                'https://boxe-mania.com/wp-content/uploads/2019/08/Rocky-Marciano.jpg',
+                'https://www.fattimarziali.it/wp-content/uploads/2020/07/roy-jones-jr.jpg',
+                'https://i.pinimg.com/originals/9c/d2/35/9cd235803feb4a951d4a8c5d45cbd7da.jpg',
+                'https://i.pinimg.com/originals/c4/41/5a/c4415abc3de2142dcb58298bb1b8fc76.jpg'
+            ]
+    },
+
+    methods:{
+        prev(){
+            if(this.counter < 0){
+                return this.counter = this.images.length-1;
+            }else{
+            return this.counter-=1;
+            }
+        },
+
+        next(){
+            if(this.counter === this.images.length){
+                return this.counter = 0;
+            }else{
+            return this.counter+=1;
+            }
+        }
+
+
+    }
+})
